9fba8a83914de1af05d2fc4a238481d5ae125e35
Adrian Viquez <aviquez96@gmail.com>
2018-09-30 22:10:41 -0700

    Integration of Index Rest Route

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require ('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// APP CONFIG
+mongoose.connect('mongodb://localhost/restful_reminder_app', {useNewUrlParser: true});
+
+// MONGOOSE/MODEL CONFIG
+var reminderSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Reminder = mongoose.model("Reminder", reminderSchema);
+
+// Reminder.create({
+//     title: "Finish Elec 301 Mini-Project",
+//     body: "This is an individual project that explores the semantics and nature of circuits."
+// }, function(err, createdReminder) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(createdReminder);
+//     }
+// })
+
+// RESTFUL ROUTES
+
+// INDEX
+app.get('/', function(req, res){
+    res.redirect("/reminders");
+})
+
+app.get('/reminders', function(req,res){
+    Reminder.find({}, function(err, remindersFound) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {reminders: remindersFound});
+        }
+    })
+})
+
+app.listen(port,console.log("Reminder server is running!"));
